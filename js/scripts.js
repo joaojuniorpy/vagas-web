5939f23b2ad2d6c1c1425396f346b24de9443606
joaojuniorpy <joaojuniorpy@gmail.com>
2020-09-11 23:34:47 -0300

    test1

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ const card = document.querySelector("div.card");
 
 const painel = [
   {
-    title: "Cargos | Salario",
+    title: "Cargo | Salario",
     tipo: "Efetivo CLT",
     image: "",
     email: ""
